4dda56b27d599574fa91e91b0b5a852699a9ceb0
pnguye38 <phuc.nguyen@cognitran.com>
2022-08-12 14:39:59 -0400

    Initial Project Add

---
+++ src/test/java/guru/spring/framework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,14 @@
+package guru.spring.framework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+}
