cd97468a50a7b96e904ab21204d43650e7d452e8
pnguye38 <phuc.nguyen@cognitran.com>
2022-09-09 13:46:02 -0400

    add specialities, added to vet. closes #29

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person
 {
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties()
+    {
+        return specialties;
+    }
+
+    public void setSpecialties(final Set<Specialty> specialties)
+    {
+        this.specialties = specialties;
+    }
 }
