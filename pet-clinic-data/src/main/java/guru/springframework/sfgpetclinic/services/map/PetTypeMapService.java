d3b38224a054900b5fa319135618181bcd7297cc
pnguye38 <phuc.nguyen@cognitran.com>
2022-09-09 14:01:52 -0400

    closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,41 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService
+{
+    @Override
+    public Set<PetType> findAll()
+    {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(final Long id)
+    {
+         super.deleteById(id);
+    }
+
+    @Override
+    public void delete(final PetType object)
+    {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(final PetType object)
+    {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(final Long id)
+    {
+        return super.findById(id);
+    }
+}
