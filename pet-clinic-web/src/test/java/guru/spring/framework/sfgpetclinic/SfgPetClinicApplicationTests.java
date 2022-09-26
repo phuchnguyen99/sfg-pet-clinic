77022ef21ff2294f24a15439af079a4cd6e265ca
pnguye38 <phuc.nguyen@cognitran.com>
2022-09-26 10:49:22 -0400

    upgraded to JUnit 5. Closes #4

--- pet-clinic-web/src/test/java/guru/spring/framework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/spring/framework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,14 +1,21 @@
 package guru.spring.framework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-//@SpringBootTest
-//class SfgPetClinicApplicationTests
-//{
-//
-//    @Test
-//    void contextLoads()
-//    {
-//    }
-//}
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = SfgPetClinicApplicationTests.class)
+public class SfgPetClinicApplicationTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+        assertThat(1, equalTo(2));
+    }
+}
